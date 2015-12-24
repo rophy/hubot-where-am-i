9dc0c03eb74e18a4102750f679073a3e2001dbf2
John Chiu <johnkchiu@yahoo.com>
2015-12-24 14:17:52 -0800

    Fix lint and test script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,32 @@
 
 module.exports = function (grunt) {
 
-  grunt.initConfig({
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          require: 'coffee-script'
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    require: 'coffee-script'
+                },
+                src: ['test/**/*.coffee', 'test/**/*.js']
+            }
         },
-        src: ['test/**/*.coffee']
-      }
-    },
-    release: {
-      options: {
-        tagName: 'v<%= version %>',
-        commitMessage: 'Prepared to release <%= version %>.'
-      }
-    },
-    watch: {
-      files: ['Gruntfile.js', 'src/**/*.coffee', 'test/**/*.coffee'],
-      tasks: ['test']
-    }
-  });
+        release: {
+            options: {
+                tagName: 'v<%= version %>',
+                commitMessage: 'Prepared to release <%= version %>.'
+            }
+        },
+        watch: {
+            files: ['Gruntfile.js', 'src/**/*.coffee', 'src/**/*.js', 'test/**/*.coffee', 'test/**/*.js'],
+            tasks: ['test']
+        }
+    });
 
-  // load all grunt tasks
-  require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
+    // load all grunt tasks
+    require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('test', ['mochaTest']);
-  grunt.registerTask('test:watch', ['watch']);
-  grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['mochaTest']);
+    grunt.registerTask('test:watch', ['watch']);
+    grunt.registerTask('default', ['test']);
 };
---
+++ test/where-am-i-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var chai = require('chai');
+var sinon = require('sinon');
+var expect = chai.expect;
+chai.use(require('sinon-chai'));
+
+describe('where-am-i', function() {
+
+    beforeEach(function() {
+        this.robot = {
+            respond: sinon.spy(),
+            hear: sinon.spy()
+        };
+        return require('../src/where-am-i')(this.robot);
+    });
+
+    it('registers a respond listener', function() {
+        expect(this.robot.respond).to.have.been.calledWith(/(wfh|pto|ooo)[\s]*(.*)/i);
+        expect(this.robot.respond).to.have.been.calledWith(/where am i/i);
+        expect(this.robot.respond).to.have.been.calledWith(/where is[\s]*([^\s]*)[\s]*([^\s]*)/i);
+    });
+});
